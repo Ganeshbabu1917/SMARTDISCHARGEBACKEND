69bb4199181cccc812e54aa26d62fc850b1ad737
GANESH BABU <163743851+Ganeshbabu1917@users.noreply.github.com>
2026-02-22 09:45:42 +0530

    Update CORS origin in ChatController

--- src/main/java/com/hospital/discharge/controller/ChatController.java
+++ src/main/java/com/hospital/discharge/controller/ChatController.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/chat")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://ec2-13-203-67-191.ap-south-1.compute.amazonaws.com:3000")
 public class ChatController {
 
     @Autowired
